cb6ef2b9178a7291436d541db61f67647add554e
lithium <119646681+lithiumDevvv@users.noreply.github.com>
2023-01-30 19:19:50 -0800

    Update print.cbl

--- print.cbl
+++ print.cbl
@@ -1,1 +1,26 @@
+       IDENTIFICATION DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       PROGRAM-ID. COBOL-PRINT.
+       ENVIRONMENT DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       CONFIGURATION SECTION.
+      *-----------------------
+       INPUT-OUTPUT SECTION.
+      *-----------------------
+       DATA DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       FILE SECTION.
+      *-----------------------
+       WORKING-STORAGE SECTION.
+      *-----------------------
+       PROCEDURE DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       MAIN-PROCEDURE.
+      **
+      * The main procedure of the program
+      **
+            DISPLAY "Cobol is dank"
+            STOP RUN.
+      ** add other procedures here
+       END PROGRAM COBOL-PRINT.
 
